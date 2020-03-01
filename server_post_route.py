8c0600a93aad32cccd936c6f72aea9f0bf27d416
SanchitAggarwal <sanchitagarwal108@gmail.com>
2020-03-01 15:22:27 +0530

    HandOn for Session 2

---
+++ server_post_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+""" A Simple Flask Server with a route
+This program is developed by Ayata Intelligence Private Limited.
+This program is proprietary software: you cannot redistribute it and/or modify it.
+"""
+
+__author__ = "Ayata Intelligence Private Limited"
+__copyright__ = "Copyright 2020, Ayata Intelligence Private Limited"
+__credits__ = ["Sanchit"]
+__date__ = "2020/03/01"
+__license__ = "proprietary"
+__maintainer__ = "Ayata Intelligence Private Limited"
+__status__ = "Development"
+__version__ = "1.0.0"
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def first_route():
+	return 'Created First Route!'
+
+if __name__ == '__main__':
+	app.run(debug=True)
